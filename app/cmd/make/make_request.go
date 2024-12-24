fe495b456678b6d4060af16f55e1e9f182aad236
wuvikr <wuvikr@qq.com>
2024-12-24 13:42:46 +0800

    make request 命令

---
+++ app/cmd/make/make_request.go
@@ -0,0 +1,25 @@
+package make
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var CmdMakeRequest = &cobra.Command{
+	Use:   "request",
+	Short: "Create a new request, e.g. make request user",
+	Run:   runMakeRequest,
+	Args:  cobra.ExactArgs(1),
+}
+
+func runMakeRequest(cmd *cobra.Command, args []string) {
+	name := args[0]
+	model := makeModelFromString(name)
+
+	// 组建目标路径
+	filepath := fmt.Sprintf("app/http/requests/%s_request.go", model.TbaleName)
+
+	// 生成文件
+	createFileFromStud(filepath, "request", model)
+}
