dc85126929f88add6d45b326c935e7bf2afd51cf
wuvikr <wuvikr@qq.com>
2025-01-02 14:22:34 +0800

    seed 命令

--- pkg/seed/seeder.go
+++ pkg/seed/seeder.go
@@ -1,6 +1,17 @@
 package seed
 
-import "gorm.io/gorm"
+import (
+	"gohub/pkg/console"
+	"gohub/pkg/database"
+
+	"gorm.io/gorm"
+)
+
+// Seeder 对应 database/seeds 目录下的 Seeder 文件
+type Seeder struct {
+	Func SeederFunc
+	Name string
+}
 
 // 存放所有 Seeder
 var seeders []Seeder
@@ -10,12 +21,6 @@ var seeders []Seeder
 var orderedSeederNames []string
 
 type SeederFunc func(*gorm.DB)
-
-// Seeder 对应 database/seeds 目录下的 Seeder 文件
-type Seeder struct {
-	Func SeederFunc
-	Name string
-}
 
 // Add 新增 Seeder到 seeders 切片中
 func Add(name string, fn SeederFunc) {
@@ -27,4 +32,46 @@ func Add(name string, fn SeederFunc) {
 
 func SetRunOrder(names []string) {
 	orderedSeederNames = names
+}
+
+func GetSeeder(name string) Seeder {
+	for _, sdr := range seeders {
+		if sdr.Name == name {
+			return sdr
+		}
+	}
+	return Seeder{}
+}
+
+func RunAll() {
+	// 先根据顺序执行
+	executed := make(map[string]string)
+	for _, name := range orderedSeederNames {
+		sdr := GetSeeder(name)
+		if len(sdr.Name) > 0 {
+			console.Warning("Running seeder: " + sdr.Name)
+			sdr.Func(database.DB)
+			executed[name] = name
+		}
+	}
+
+	// 再执行没有顺序的
+	for _, sdr := range seeders {
+		if _, ok := executed[sdr.Name]; !ok {
+			console.Warning("Running seeder: " + sdr.Name)
+			sdr.Func(database.DB)
+		}
+	}
+
+}
+
+// 运行指定的 seeder
+func RunSeeder(name string) {
+	for _, sdr := range seeders {
+		if sdr.Name == name {
+			console.Warning("Running seeder: " + sdr.Name)
+			sdr.Func(database.DB)
+			return
+		}
+	}
 }
