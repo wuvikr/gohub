899183b67f1cf88c2238bd80dd0b04ab7294a4f1
wuvikr <wuvikr@qq.com>
2025-01-14 14:51:38 +0800

    更新分类

--- app/http/controllers/api/v1/categories_controller.go
+++ app/http/controllers/api/v1/categories_controller.go
@@ -30,3 +30,30 @@ func (ctrl *CategoriesController) Store(c *gin.Context) {
 		response.Abort500(c, "创建失败，请稍后尝试~")
 	}
 }
+
+// Update 更新分类
+func (ctrl *CategoriesController) Update(c *gin.Context) {
+	// 1. 验证参数 id 对应的分类是否存在
+	categoryModel := category.Get(c.Param("id"))
+	if categoryModel.ID == 0 {
+		response.Abort404(c)
+		return
+	}
+
+	// 2. 验证表单
+	request := requests.CategoryRequest{}
+	if ok := requests.Validate(c, &request, requests.CategorySave); !ok {
+		return
+	}
+
+	// 3. 更新分类
+	categoryModel.Name = request.Name
+	categoryModel.Description = request.Description
+	rowsAffected := categoryModel.Save()
+
+	if rowsAffected > 0 {
+		response.Data(c, categoryModel)
+	} else {
+		response.Abort500(c)
+	}
+}
